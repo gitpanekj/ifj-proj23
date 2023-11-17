cb49525156f1d37c1c9b01231465509433730e1c
Lukas Kotoun <lukas.kotoun107@seznam.cz>
2023-11-17 22:20:45 +0100

    edit test files

--- tests/parserSourceFiles/ifjExamples/example3.swift
+++ tests/parserSourceFiles/ifjExamples/example3.swift
@@ -1,7 +1,7 @@
 /* Program 3: Prace s retezci a vestavenymi funkcemi */
 
-var str1 = 5
-let str2 = "Toto je nejaky text v programu jazyka IFJ23" + ", ktery jeste trochu obohatime"
+var str1 = "Toto je nejaky text v programu jazyka IFJ23"
+let str2 = str1 + ", ktery jeste trochu obohatime"
 write(str1, "\n", str2, "\n")
 let i = length(str1)
 write("Delka retezce ve str1: ", i, "\n")
@@ -10,9 +10,9 @@ write("Zadejte serazenou posloupnost vsech malych pismen a-h, ")
 let newInput = readString()
 if let newInput {
 	str1 = newInput
-	while (5 != 5) {
+	while (str1 != "abcdefgh") {
 		write("Spatne zadana posloupnost, zkuste znovu:\n")
 		let tmp = readString()
-	
+		str1 = tmp ?? ""
 	}
 }else{}
