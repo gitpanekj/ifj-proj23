07e3a9065b67785ef24dcb7f7c5682fcea69b32c
Lukas Kotoun <lukas.kotoun107@seznam.cz>
2023-11-24 09:13:55 +0100

    new tests

---
+++ tests/parserSourceFiles/syntax/whileTests/compexWhile.swift
@@ -0,0 +1,20 @@
+var i = 5
+var x = 6
+
+while(i < 9){
+i = i+1
+ let a :Double? = 5
+ if let a {
+    write("x")
+ } else{}
+}
+while(x < 10){
+x = x+1
+ let a :Double? = nil
+ if let a {
+    write("x")
+ } else{
+     write("y")
+ }
+
+}
---
+++ tests/parserSourceFiles/syntax/whileTests/whiles.swift
@@ -0,0 +1,13 @@
+var i = 0
+while(i<=5){
+    var b : Double
+    var c : Double
+    var r = 156
+    var s : Double
+    var q : Double
+    var l : Double = 123
+    var v : Double
+    var n : Double
+    write(r, "\n")
+    write(l, "\n")
+}
